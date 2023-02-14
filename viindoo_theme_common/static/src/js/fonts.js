d01d6b684ab2defa10c2befc999c391164b2af81
SonCrits <truongson290893@gmail.com>
2023-02-14 11:32:46 +0700

    delete moi thu

--- viindoo_theme_common/static/src/js/fonts.js
+++
@@ -1,5 +0,0 @@
-odoo.define('viindoo_theme_common.wysiwyg.fonts', function(require) {
-	const { fontIcons } = require('wysiwyg.fonts');
-
-	fontIcons.push({ base: 'fa', parser: /\.(icon-viin-(?:\w|-)+)::?before/i });
-});
